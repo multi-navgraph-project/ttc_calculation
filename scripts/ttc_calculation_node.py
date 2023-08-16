2b5f130d48be42029fa79afd32c1acec351db67a
LotfiZ <zeghmilot@gmail.com>
2023-08-16 17:23:03 -0400

    change queue size + format quat type

--- scripts/ttc_calculation_node.py
+++ scripts/ttc_calculation_node.py
@@ -4,6 +4,8 @@ import rospy
 import ttc_calculation
 import pandas as pd
 import math
+from tabulate import tabulate
+import numpy as np
 from nav_msgs.msg import Odometry
 import tf
 
@@ -17,10 +19,10 @@ class TTCCalculationNode:
         'x_j', 'y_j', 'vx_j', 'vy_j', 'hx_j', 'hy_j','length_j', 'width_j'])
         
         # Subscriber for ego robot information
-        self.ego_robot_sub = rospy.Subscriber('/agv_ego/odom', Odometry, self.ego_robot_callback)
+        self.ego_robot_sub = rospy.Subscriber('/agv_1/odom', Odometry, self.ego_robot_callback,queue_size=1)
         
         # Subscriber for other robot's information
-        self.other_robot_sub = rospy.Subscriber('/obstacle_n/', Odometry, self.other_robot_callback)
+        self.other_robot_sub = rospy.Subscriber('/agv_2/odom', Odometry, self.other_robot_callback,queue_size=1)
 
         # Initialize variables to store robot information
         self.ego_robot_data = None
@@ -29,30 +31,39 @@ class TTCCalculationNode:
     def ego_robot_callback(self, data):
         # Store the received ego robot information
         self.ego_robot_data = data
+
+        orientation = self.ego_robot_data.pose.pose.orientation
+        quaternion = np.array([orientation.x, orientation.y, orientation.z, orientation.w])
        
-        _ , _, yaw = tf.transformations.euler_from_quaternion(self.ego_robot_data.pose.pose.orientation)
+        _ , _, yaw = tf.transformations.euler_from_quaternion(quaternion)
         x_i = self.ego_robot_data.pose.pose.position.x
         y_i = self.ego_robot_data.pose.pose.position.y
         vx_i = self.ego_robot_data.twist.twist.linear.x * math.cos(yaw)
         vy_i = self.ego_robot_data.twist.twist.linear.x * math.sin(yaw)
         hx_i = math.cos(yaw)
         hy_i = math.sin(yaw)
-        length_i = 1.8
-        width_i = 1.5
+        length_i = 2
+        width_i = 2
 
-        _ , _, yaw = tf.transformations.euler_from_quaternion(self.other_robot_data.pose.pose.orientation)
+        orientation = self.other_robot_data.pose.pose.orientation
+        quaternion = np.array([orientation.x, orientation.y, orientation.z, orientation.w])
+
+        _ , _, yaw = tf.transformations.euler_from_quaternion(quaternion)
         x_j = self.other_robot_data.pose.pose.position.x
         y_j = self.other_robot_data.pose.pose.position.y
         vx_j = self.other_robot_data.twist.twist.linear.x * math.cos(yaw)
         vy_j = self.other_robot_data.twist.twist.linear.x * math.sin(yaw)
         hx_j = math.cos(yaw)
         hy_j = math.sin(yaw)
-        length_j = 1.8
-        width_j = 1.5
+        length_j = 2
+        width_j = 2
+
+
 
         self.data = {'x_i' : x_i, 'y_i' : y_i, 'vx_i' : vx_i, 'vy_i' : vy_i, 'hx_i' : hx_i , 'hy_i' : hy_i,'length_i' : length_i, 'width_i' : width_i,
         'x_j' : x_j, 'y_j' : y_j, 'vx_j' : vx_j, 'vy_j' : vy_j, 'hx_j' : hx_j, 'hy_j' : hy_j,'length_j' : length_j, 'width_j': width_j}
 
+        self.data = pd.DataFrame([self.data])
 
         # Try to calculate TTC if both ego and other robot data are available
         self.calculate_ttc(self.data)
@@ -65,7 +76,7 @@ class TTCCalculationNode:
         # Ensure we have received both ego and other robot data before calculating TTC
         if self.ego_robot_data is not None and self.other_robot_data is not None:
             result = ttc_calculation.TTC(input, 'dataframe')
-            print("Great Results...or not...", result)
+            print(tabulate(result, headers='keys', tablefmt='psql'))
         else:
             rospy.logdebug("No Data")
     
